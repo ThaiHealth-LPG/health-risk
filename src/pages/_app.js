3a12d75d662bc6db8c0521242095cf3ac7a1b8d4
bosch <130572668+boschaiyadej@users.noreply.github.com>
2024-08-19 11:08:03 +0700

    feat: setup project

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,23 @@
 import "@/styles/globals.css";
+import { ChakraProvider } from "@chakra-ui/react";
+import { Kanit, Sarabun } from "@next/font/google";
+
+const kanit = Kanit({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+});
+
+const sarabun = Sarabun({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+});
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <ChakraProvider>
+      <main className={`${kanit.className} ${sarabun.className}`}>
+        <Component {...pageProps} />
+      </main>
+    </ChakraProvider>
+  );
 }
